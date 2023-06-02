e43dc1bee5a1bb91b7323cf294562821ec5a86de
Jafar <jafarcees@gmail.com>
2023-06-02 13:55:03 +0400

    seed file changes

--- libs/api/data-access-db/src/lib/seed/seeddata.ts
+++ libs/api/data-access-db/src/lib/seed/seeddata.ts
@@ -88,6 +88,65 @@ export const categoryData: Prisma.CategoryCreateInput[] = [
       ],
     },
   },
+  {
+    name: 'Fish',
+    color: '#fefeef',
+    kitchen: {
+      create: {
+        name: 'SecondK',
+        printer: 'Main',
+      },
+    },
+    isAvailableAllDay: true,
+    startTime: '10 pm',
+    endTime: '11am',
+    products: {
+      create: [
+        {
+          name: 'Fish Chilli',
+          secondaryLanguageName: 'الفلفل الحار السمك',
+          collectionId: 4,
+          cost: 1.6,
+          price: 1.6,
+        },
+        {
+          name: 'Fish Masala',
+          secondaryLanguageName: 'سمك ماسالا',
+          collectionId: 4,
+          cost: 1.6,
+          price: 1.6,
+        },
+        {
+          name: 'Fish Manchurian',
+          secondaryLanguageName: 'الأسماك manchurian',
+          collectionId: 4,
+          cost: 1.6,
+          price: 1.6,
+        },
+        {
+          name: 'Fish Fry',
+          secondaryLanguageName: 'سمك مقلي',
+          collectionId: 4,
+          cost: 1.8,
+          price: 1.8,
+        },
+        {
+          name: 'Squid',
+          secondaryLanguageName: 'حبار',
+          collectionId: 4,
+          cost: 1.5,
+          price: 1.5,
+        },
+        {
+          name: 'Crab',
+          secondaryLanguageName: 'سلطعون',
+          collectionId: 4,
+          cost: 1.6,
+          price: 1.6,
+        },
+      ],
+    },
+  },
 ];
 
 export const data = {
