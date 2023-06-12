402037344580df03c0324688f85e8b16c8c328be
Jafar <jafarcees@gmail.com>
2023-06-12 14:26:27 +0400

    back office side nav layout done

--- libs/orderapp/backoffice/feature/page/src/lib/backoffice.routes.ts
+++ libs/orderapp/backoffice/feature/page/src/lib/backoffice.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { OrderappBackofficeFeaturePageComponent } from './orderapp-backoffice-feature-page/orderapp-backoffice-feature-page.component';
 
 export const shellRoutes: Routes = [
-  { path: '', component: OrderappBackofficeFeaturePageComponent },
   {
-    path: 'product',
-    loadComponent: () =>
-      import('@hotel/orderapp/product/feature/add').then(
-        (c) => c.OrderappProductFeatureAddComponent
-      ),
+    path: '',
+    component: OrderappBackofficeFeaturePageComponent,
+    children: [
+      {
+        path: 'product',
+        loadComponent: () =>
+          import('@hotel/orderapp/product/feature/add').then(
+            (c) => c.OrderappProductFeatureAddComponent
+          ),
+      },
+    ],
   },
 ];
--- libs/orderapp/backoffice/feature/page/src/lib/orderapp-backoffice-feature-page/orderapp-backoffice-feature-page.component.ts
+++ libs/orderapp/backoffice/feature/page/src/lib/orderapp-backoffice-feature-page/orderapp-backoffice-feature-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'hotel-orderapp-backoffice-feature-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   templateUrl: './orderapp-backoffice-feature-page.component.html',
   styleUrls: ['./orderapp-backoffice-feature-page.component.css'],
 })
