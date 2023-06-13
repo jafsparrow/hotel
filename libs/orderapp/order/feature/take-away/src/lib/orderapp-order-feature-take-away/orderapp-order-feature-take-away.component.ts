d423d6bdc60284a8f5d137c462274abdc04771b9
Jafar <jafarcees@gmail.com>
2023-06-13 18:05:25 +0400

    take away form updated

--- libs/orderapp/order/feature/take-away/src/lib/orderapp-order-feature-take-away/orderapp-order-feature-take-away.component.ts
+++ libs/orderapp/order/feature/take-away/src/lib/orderapp-order-feature-take-away/orderapp-order-feature-take-away.component.ts
@@ -4,28 +4,38 @@ import { User, UserType } from '@hotel/orderapp/shared/data-access';
 import { Store } from '@ngrx/store';
 import { setCartCreatedForUser } from '@hotel/orderapp/cart/data-access';
 import { MatDialogRef } from '@angular/material/dialog';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'hotel-orderapp-order-feature-take-away',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './orderapp-order-feature-take-away.component.html',
   styleUrls: ['./orderapp-order-feature-take-away.component.css'],
 })
 export class OrderappOrderFeatureTakeAwayComponent {
+  takeAwayForm: FormGroup = new FormGroup({
+    customerName: new FormControl('', Validators.required),
+    otherDetails: new FormControl(''),
+  });
   constructor(
     private store: Store,
     private dialogRef: MatDialogRef<OrderappOrderFeatureTakeAwayComponent>
   ) {}
-  customerName = '';
-  otherDetails = '';
 
   setTheUserToCart() {
     const user: User = {
-      firstName: 'Hameed',
-      lastName: 'Car 1022',
+      firstName: this.takeAwayForm.value.customerName,
+      lastName: this.takeAwayForm.value.otherDetails,
       userType: UserType.TAKEAWAY,
     };
+
+    console.log(this.takeAwayForm.value);
 
     this.store.dispatch(setCartCreatedForUser({ user }));
     this.dialogRef.close();
