aefda99315b766385226af3f93cfbac9c4b38adc
Jafar <jafarcees@gmail.com>
2023-06-22 00:21:16 +0400

    order list ui changes

--- libs/orderapp/order/feature/page/src/lib/orderapp-order-feature-page/orderapp-order-feature-page.component.ts
+++ libs/orderapp/order/feature/page/src/lib/orderapp-order-feature-page/orderapp-order-feature-page.component.ts
@@ -5,12 +5,24 @@ import { OrderappOrderFeatureOrderDetailComponent } from '@hotel/orderapp/order/
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { loadRecentOrders } from '@hotel/orderapp/order/data-access/order';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { logout } from '@hotel/orderapp/auth/data-access';
 
 @Component({
   selector: 'hotel-orderapp-order-feature-page',
   standalone: true,
   imports: [
     CommonModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
     OrderappOrderFeatureOrderListComponent,
     OrderappOrderFeatureOrderDetailComponent,
     MatDialogModule,
@@ -29,5 +41,9 @@ export class OrderappOrderFeaturePageComponent {
       height: '100vh',
       width: '100vw',
     });
+  }
+
+  logout() {
+    this.store.dispatch(logout());
   }
 }
